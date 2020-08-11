2f6dba96041c44d1c5409747608eef588547cbbe
branata007 <branata007@gmail.com>
2020-08-12 00:59:54 +0800

    Update example's code

--- app/src/main/java/com/nata/widgetexample/MainActivity.kt
+++ app/src/main/java/com/nata/widgetexample/MainActivity.kt
@@ -2,10 +2,13 @@ package com.nata.widgetexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ciw_name_big.setInitial("Clarissa Eliani Sutikno")
+        ciw_name_big.setBackgroundColor("#ACD4EF")
     }
 }
